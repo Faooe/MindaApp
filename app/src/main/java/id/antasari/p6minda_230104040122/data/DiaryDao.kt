88281dc34533453b788089a66c40d485cfefb01c
Faooe <nfarinn1@gmail.com>
2025-11-12 17:44:15 +0800

    pertama

---
+++ app/src/main/java/id/antasari/p6minda_230104040122/data/DiaryDao.kt
@@ -0,0 +1,27 @@
+package id.antasari.p6minda_230104040122.data
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface DiaryDao {
+
+    // --- TAMBAHKAN FUNGSI BARU DI BAWAH INI ---
+    @Query("SELECT * FROM diary_entries ORDER BY timestamp DESC")
+    fun observeAll(): Flow<List<DiaryEntry>> //
+
+    @Query("SELECT * FROM diary_entries ORDER BY timestamp DESC")
+    suspend fun getAll(): List<DiaryEntry>
+
+    @Query("SELECT * FROM diary_entries WHERE id = :entryId LIMIT 1")
+    suspend fun getById(entryId: Int): DiaryEntry?
+
+    @Insert
+    suspend fun insert(entry: DiaryEntry): Long
+
+    @Update
+    suspend fun update(entry: DiaryEntry)
+
+    @Delete
+    suspend fun delete(entry: DiaryEntry)
+}
